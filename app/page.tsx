adf9737a0b13a26511ee0d03384a934fd67cab93
CallumNewman <ballumnew@gmail.com>
2024-09-29 16:25:27 +0100

    Created initial layout of home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,19 @@ import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Callum Newman</h1>
-      <ThemeToggle />
+    <main className="flex gap-10 w-full h-full flex-1">
+      {/* Left Side */}
+      <div className="flex-1 p-6 h-full max-w-md">
+        <div className="dark:bg-emerald-700 rounded-md h-full flex items-center justify-center items-center">
+          Left Side
+        </div>
+      </div>
+      {/* Right Side */}
+      <div className="flex-1 p-6 h-full w-full gap-10">
+        <div className="dark:bg-emerald-700 rounded-md h-full flex items-center justify-center items-center">
+          Right Side
+        </div>
+      </div>
     </main>
   );
 }
