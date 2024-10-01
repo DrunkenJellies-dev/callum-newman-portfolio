c00a584bf919f44de000aeecd8324849de3b1cb2
CallumNewman <ballumnew@gmail.com>
2024-10-01 20:26:36 +0100

    Added Button component and added it to the social box

--- components/grid-items/social-box.tsx
+++ components/grid-items/social-box.tsx
@@ -1,6 +1,7 @@
 import { GridItemInterface } from "@/config/site-config";
 import React from "react";
 import Icon from "../icon";
+import Button from "../button";
 
 const SocialBox = ({ item }: { item: GridItemInterface }) => {
   return (
@@ -8,9 +9,15 @@ const SocialBox = ({ item }: { item: GridItemInterface }) => {
       {/* Header */}
       <div className="flex items-center justify-between">
         {/* Icon */}
-        <Icon type={item.icon ?? ""} color={item.color}/>
+        <Icon type={item.icon ?? ""} color={item.color} />
         {/* Button 2x2 */}
-        {item.layout === "2x2" && <div>Button</div>}
+        {item.layout === "2x2" && (
+          <Button
+            text={item.buttonTitle ?? ""}
+            secondaryText={item.buttonSecondaryText ?? ""}
+            color={item.color ?? ""}
+          />
+        )}
       </div>
       {/* Content Container */}
       <div className="mt-2">
@@ -20,11 +27,21 @@ const SocialBox = ({ item }: { item: GridItemInterface }) => {
         <div className="text-sm text-neutral-500">{item.username}</div>
         {/* Description */}
         {item.description && (
-          <div className="text-sm text-neutral-500 mt-1">{item.description}</div>
+          <div className="text-sm text-neutral-500 mt-1 line-clamp-2">
+            {item.description}
+          </div>
         )}
       </div>
       {/* Button 1x2 */}
-      {item.layout === "1x2" && <div>Button</div>}
+      {item.layout === "1x2" && (
+        <div className="mt-2">
+          <Button
+            text={item.buttonTitle ?? ""}
+            secondaryText={item.buttonSecondaryText ?? ""}
+            color={item.color ?? ""}
+          />
+        </div>
+      )}
     </div>
   );
 };
